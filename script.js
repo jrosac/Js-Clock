bc3a9a232eeea6bfce09fdf5e4a449f82755b79d
jrosac <joaorosa.conceicao@dcomp.ufs.br>
2024-03-11 13:20:19 -0300

    Added hour and hour degrees to the setDate function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const secondHand = document.querySelector('.second-hand');
 const minsHand   = document.querySelector('.min-hand');
+const hourHand  = document.querySelector('.hour-hand')
 
 function setDate() {
     const now = new Date();
@@ -10,11 +11,14 @@ function setDate() {
     console.log(seconds)
 
     const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    const minsDegrees    = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
     console.log(mins)
 
-    
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    console.log(hour)
     
 }
 setInterval(setDate,1000);
