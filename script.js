9f78abd357cc3bb741f562980f1dca5db318267d
jrosac <joaorosa.conceicao@dcomp.ufs.br>
2024-03-11 13:21:25 -0300

    Put the console logs used to the debug in a comment style

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ function setDate() {
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds)
+    /*console.log(seconds)*/
 
     const mins = now.getMinutes();
     const minsDegrees    = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-    console.log(mins)
+    /*console.log(mins)*/
 
     const hour = now.getHours();
     const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-    console.log(hour)
+    /*console.log(hour)*/
     
 }
 setInterval(setDate,1000);
