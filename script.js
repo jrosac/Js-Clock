4ddaf9e136cfd7e5c4803de96412e3ab76eff200
jrosac <joaorosa.conceicao@dcomp.ufs.br>
2024-03-11 12:55:50 -0300

    Added the function setDate and added the seconds degrees to it

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const secondHand = document.querySelector('.second-hand')
+
 function setDate(){
-    console.log("hi")
-    }
+    /*console.log("hi");*/
+    const now = new Date();
+    const seconds = now.getSeconds;
+    const secondsDegrees = ((seconds/60) * 360);
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    console.log(seconds);
+    
+}
 setInterval(setDate,1000);
